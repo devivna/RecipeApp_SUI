656306b84d32ebc5f6e4ea6c212af2cf225f4c56
Nadia <ivashchenko.nadia@gmail.com>
2022-12-17 18:18:44 +0200

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Students on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
